80d36a682c0ed8bfaab77f95a0165af459a40705
NEYTULLAH BAHADIR <nate-nate86@hotmail.com>
2022-04-13 21:54:39 +0300

    page is about istanbul

---
+++ assets/img/img-link.js
@@ -0,0 +1,35 @@
+const imgLightLinks = {
+  background: { author: '', link: 'https://unsplash.com/photos/-HxENS1GYSA' },
+  0: 'https://pixabay.com/photos/istanbul-panoramic-appearance-1261194/',
+  1: 'https://pixabay.com/photos/cloudy-greens-sea-blue-beautiful-1402190/',
+  2: 'https://pixabay.com/photos/bridge-river-skyline-city-6835467/',
+  events: [
+    {
+      title: ' Turkish food and market tour',
+      link:
+        'https://images.unsplash.com/photo-1565560665129-4831aa15206c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80'
+    },
+    {
+      title: 'Palace Walking Tour',
+      link:
+        'https://images.unsplash.com/photo-1531257240678-d5b1922e672d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
+    }
+  ]
+};
+const imgDarkLinks = {
+  background: { author: "", link: 'https://unsplash.com/photos/qegMLAiTBA4' },
+  0: 'https://pixabay.com/images/id-1215000/',
+  1: 'https://pixabay.com/images/id-2162772/',
+  2: 'https://pixabay.com/images/id-2805540/',
+  events: [
+    {
+      title: 'dinner on the cruise',
+      link: 'https://images.unsplash.com/photo-1517166988974-5e377975dd67?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+      title: 'nightsight walking and talk with beer',
+      link:
+        'https://images.unsplash.com/photo-1515250331755-a666fc377a49?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+    }
+  ]
+};
